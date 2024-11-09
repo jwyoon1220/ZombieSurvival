217ed52cba1a72a53e72625fbdfc0139d252307c
jwyoon1220 <jwyoon1220@naver.com>
2024-11-09 18:58:00 +0900

    치지직 봇 기능 포함 서버

--- src/main/kotlin/io/jwyoon1220/survival/plugin/SurvivalPlugin.kt
+++ src/main/kotlin/io/jwyoon1220/survival/plugin/SurvivalPlugin.kt
@@ -65,53 +65,37 @@ class SurvivalPlugin : JavaPlugin() {
             world.difficulty = Difficulty.HARD
         }
     }
-
     override fun onDisable() {
         chat.closeAsync()
     }
     fun createVaccine(): ItemStack {
         // ItemStack 생성 (물약으로 만들기 위해 Material를 POTION으로 설정)
         val vaccine = ItemStack(Material.LINGERING_POTION, 1)
-
-
         // PotionMeta 생성 및 설정
         val meta = vaccine.itemMeta as PotionMeta
         meta.setDisplayName("§a좀비 바이러스 백신(가스)") // 이름 설정
-
-
         // 설명 추가
         meta.lore = Arrays.asList(
             "§7이 포션은 좀비 바이러스의",
             "§7감염을 예방하고 증상을 완화합니다.",
             "§7사용 시 공격력이 감소합니다."
         )
-
-
         // 나약함 효과 추가
         meta.addCustomEffect(PotionEffect(PotionEffectType.WEAKNESS, 600, 5, true), true)
-
-
         // 메타데이터를 ItemStack에 적용
         vaccine.setItemMeta(meta)
-
         return vaccine // 완성된 ItemStack 반환
     }
-
-
     private fun addVaccineRecipe() {
         // 커스텀 아이템 생성 (네더의 별)
         val vaccineItem = ItemStack(Material.GHAST_TEAR)
-
         // 아이템 메타 설정
         val meta: ItemMeta = vaccineItem.itemMeta ?: return
         meta.setDisplayName("${ChatColor.GREEN}좀비 바이러스 백신(고(固)형제)") // 아이템 이름
         meta.lore = listOf("백신이지만 치료제도 된다고 한다.", "뭔가 맛이 매우 없을거 같다.") // 아이템 설명
         vaccineItem.itemMeta = meta
-
         // 레시피 키 생성
         val key = NamespacedKey(this, "vaccine")
-
-
         val recipe = ShapelessRecipe(key, vaccineItem).apply {
             addIngredient(Material.GOLD_INGOT)    // 금괴
             addIngredient(Material.GHAST_TEAR)     // 네더의 별
@@ -119,22 +103,9 @@ class SurvivalPlugin : JavaPlugin() {
             addIngredient(Material.MILK_BUCKET)      // 우유 양동이
             addIngredient(Material.DIAMOND)          // 다이아몬드
         }
-
-        val key2 = NamespacedKey(this, "vaccine_gas")
-
-
-        val recipe2 = ShapelessRecipe(key2, createVaccine()).apply {
-            addIngredient(Material.WATER_BUCKET)
-            addIngredient(vaccineItem)
-
-        }
         // 레시피 등록
         server.addRecipe(recipe)
-        //server.addRecipe(recipe2)
-
         // 서버 메시지 출력
         logger.info("백신 제작법이 추가되었습니다!")
     }
-
-
 }
